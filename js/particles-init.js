3ae3d6043e371eaa08e2e268e32e203ca2365da9
Asier <97402868+parzival-von@users.noreply.github.com>
2025-04-21 20:00:11 +0200

    Seccion sobre mi practicamente acabada, no funcionan las particulas

---
+++ js/particles-init.js
@@ -0,0 +1,6 @@
+// Carga la configuración (ajusta ruta si tu JSON está en otra carpeta)
+particlesJS.load(
+  'particles-js',
+  '../js/particles-config.json',
+  () => console.log('Particles cargadas')
+);
